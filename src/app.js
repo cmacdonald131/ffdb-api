84f771c10985e6776113c643e75174e14e6a5cef
Christopher MacDonald <cmacdonald131@gmail.com>
2020-02-24 20:19:29 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
 
 const app = express()
 
@@ -14,20 +16,13 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(validateBearerToken)
+
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('Time to win!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message.error }
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app
