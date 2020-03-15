e926b706d4410247a30c9f7211b65da9b1d6e601
Christopher MacDonald <cmacdonald131@gmail.com>
2020-03-15 09:30:59 -0400

    typo correction

--- migrations/001.undo.create_ffdb_users.sql
+++ migrations/001.undo.create_ffdb_users.sql
@@ -1,4 +1,4 @@
-ALTER TABLE blogful_articles
-  DROP COLUMN IF EXISTS author_id;
+ALTER TABLE ffdb_users
+  DROP COLUMN IF EXISTS user_id;
 
-DROP TABLE IF EXISTS blogful_users;
+DROP TABLE IF EXISTS ffdb_users;
